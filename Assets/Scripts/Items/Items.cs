c6cc98b98b27382e33e5c266e48158dc8787abda
Lucsc <luc1.schmitt@epitech.eu>
2023-11-16 20:20:43 -0500

    Change script for items interface

---
+++ Assets/Scripts/Items/Items.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+
+    public GameObject UI;
+    public DialogueTrigger trigger;
+    private bool inRange;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inRange = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(inRange)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                trigger.TriggerDialogueTree();
+            }
+        }
+    }
+}
